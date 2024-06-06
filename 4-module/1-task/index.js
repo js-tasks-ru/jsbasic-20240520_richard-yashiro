21220ff0100e6c2a41dc990c82b1648621753291
Андрей Сысоев <AisakaTaigak@mail.ru>
2024-06-06 16:25:35 +0300

    описание

---
+++ 4-module/1-task/index.js
@@ -0,0 +1,15 @@
+function makeFriendsList(friends) {
+
+  const ul = document.createElement('ul');
+
+  friends.forEach(friend => {
+    const li = document.createElement('li');
+
+    li.textContent = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+
+   
+  });
+
+  return ul;
+}
